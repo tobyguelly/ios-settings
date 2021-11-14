8ab53a77062f29a486bb42cb9641a5f7d4165c4a
Toby <tobias.guelly@gmx.at>
2021-11-14 17:07:11 +0100

    Initial Commit

---
+++ Settings/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Settings
+//
+//  Created by Tobias Gülly on 14.11.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Settings/SettingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SettingsApp.swift
+//  Settings
+//
+//  Created by Tobias Gülly on 14.11.21.
+//
+
+import SwiftUI
+
+@main
+struct SettingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
